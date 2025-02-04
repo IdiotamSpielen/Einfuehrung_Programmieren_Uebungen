698a9e17affdb3408c77f2e3fddd0b15bccec33d
IdiotamSpielen <105610621+IdiotamSpielen@users.noreply.github.com>
2025-02-04 12:57:16 +0100

    Update MorseCode.cpp

--- Modul Komplexe Datentypen/Übungsaufgabe Morsecode/C++/Uebungsaufgabe_Morsecode_MK/MorseCode.cpp
+++ Modul Komplexe Datentypen/Übungsaufgabe Morsecode/C++/Uebungsaufgabe_Morsecode_MK/MorseCode.cpp
@@ -1,56 +1,38 @@
 #include <iostream>
 #include <map>
-#include <algorithm>
 using namespace std;
 
 //Primary:
-//[x] Entwickle ein Programm, das Buchstaben in Morsecode übersetzt
+//[ ] Entwickle ein Programm, das Buchstaben in Morsecode übersetzt
 //Speichern Sie hierzu einfach einen Buchstaben in einer Variablen und lassen Sie diesen sodann übersetzten.
+//Die Morsecode übersetzungstabelle befindet sich hier: https://en.wikipedia.org/wiki/Morse_code#/media/File:International_Morse_Code.svg
 //Secondary:
-//[x] Übersetzung ganzer wörter
+//[ ] Übersetzung ganzer wörter
 //[ ] Übersetzung ganzer sätze
-
-string translate(const string& str, const map<char, string>& morse_codes){
-    string translated;
-    for (char c : str){
-        if (morse_codes.count(c)){
-            translated.append(morse_codes.at(c));
-        }
-    }
-    return translated;
-}
-
-string CAPSLOCK(const string& str, map<char, string>& morse_codes){  
-    string stringCaps;
-    for (char c : str){
-        c = toupper(c);
-        stringCaps.push_back(c);
-    }
-    return stringCaps;
-}
 
 int main()
 {
+    char letter;
     map<char, string> morse_codes = {
         {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."}, {'F', "..-."},
         {'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."},
         {'M', "--"}, {'N', "-."}, {'O', "---"}, {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."},
         {'S', "..."}, {'T', "-"}, {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"},
-        {'Y', "-.--"}, {'Z', "--.."}, {' ', "   "},
+        {'Y', "-.--"}, {'Z', "--.."},
         {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"}, {'5', "....."},
         {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."}, {'0', "-----"}
     };
+    cout << "Welchen Buchstaben möchten sie Uebersetzen?" << endl;
+    cin >> letter;
 
-    string toTranslate;
-    cout << "Was wollen Sie Uebersetzen?" << endl;
-    cin >> toTranslate;
+    if (morse_codes.count(letter))
+    {
+        cout << morse_codes.at(letter);
+    }
+    
+    cout << letter;
 
-    cout << "Die Eingabe:" << toTranslate << endl;
 
-    toTranslate = CAPSLOCK(toTranslate, morse_codes);
-    string translated = translate(toTranslate, morse_codes);
-
-    cout << "wird uebersetzt als:" << translated << endl;
 
     return 0;
 }
